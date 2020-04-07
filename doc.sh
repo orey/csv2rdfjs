a6383cfce36c0ae169e0d7bd5e1d33d971b77848
Olivier <olivier@debian>
2020-04-07 07:52:15 +0200

    adding stuff

---
+++ doc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./node_modules/.bin/jsdoc -d ./doc csv2rdf.js
+
+
--- test.js
+++ test.js
@@ -13,14 +13,14 @@ const assert = require("assert");
 
 const testHeader = "DS origin;DS Number;DS Type;DS Version;DS Name;DS State;DS Part Classification";
 
-const testPatterns = ["c3", "c3+c4", "c4+c3+c12", "c4+R", "u3+u4+R", "$foo_+c3+u4+R+c7",
+const testPatterns = ["c3", "c3+c4", "c4+c3+c12", "c4+c1", "u3+u4", "$foo_+c3+u4+c7",
                       "c3+$foo+c12+$bar", "u3+c4*+$bar", "$blah+c7+u4+_ho"];
 
 function test01() {
     let tab = testHeader.split(";");
     for (var i=0;i< testPatterns.length;i++) {
         try {
-            console.log(C2R.transform(tab, testPatterns[i], 456));
+            console.log(C2R.pkeyParser(tab, testPatterns[i], 456));
         }
         catch (e){
             console.error(e);
